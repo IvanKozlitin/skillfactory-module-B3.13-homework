d9687f00af5787514a90472d36243f837597f84a
KI-Shadow <ivankozlitin2012@gmail.com>
2020-10-20 14:47:32 +0300

    Fixed bags in class HTML

--- my_program.py
+++ my_program.py
@@ -1,55 +1,46 @@
-# Разработанный код
-
-class HTML():
+class HTML:
+    """Класс HTML файла"""
     def __init__(self, output):
+        self.tag = "HTML"
+        self.html_code = ""
         self.output = output
 
     def __enter__(self):
         return self
 
-class TopLevelTag():
-    def __init__(self, top_level_tag_name):
-        self.top_level_tag_name = top_level_tag_name
-        self.attributes = {}
+    def __exit__(self, type, value, traceback):
+        with open(self.output, 'w') as result_file:
+            self.html_code = "<{tag}>\n{html_code}\n</{tag}>".format(tag=self.tag, html_code=self.html_code)
+            result_file.write(self.html_code)
 
-    def __enter__(self):
-        return self
+    def __iadd__(self, other):
+        self.html_code += other.html_code
 
-class Tag():
-    def __init__(self, tag_name, is_single = False):
-        self.tag_name = tag_name
-        self.text = ""
-        self.attributes = {}
-        self.is_single = is_single
-
-    def __enter__(self):
-        return self
-
-# Доработать классы и разобраться с тем как это всё работает!!!
 
 
 # Исходник
 
 with HTML(output="test.html") as doc:
-    with TopLevelTag("head") as head:
-        with Tag("title") as title:
-            title.text = "hello"
-            head += title
-        doc += head
+    pass
+    # with TopLevelTag("head") as head:
+    #     with Tag("title") as title:
+    #         title.text = "hello"
+    #         head += title
+    #     doc += head
 
-    with TopLevelTag("body") as body:
-        with Tag("h1", klass=("main-text",)) as h1:
-            h1.text = "Test"
-            body += h1
+    # with TopLevelTag("body") as body:
+    #     with Tag("h1", klass=("main-text",)) as h1:
+    #         h1.text = "Test"
+    #         body += h1
 
-        with Tag("div", klass=("container", "container-fluid"), id="lead") as div:
-            with Tag("p") as paragraph:
-                paragraph.text = "another test"
-                div += paragraph
+    #     with Tag("div", klass=("container", "container-fluid"), id="lead") as div:
+    #         with Tag("p") as paragraph:
+    #             paragraph.text = "another test"
+    #             div += paragraph
 
-            with Tag("img", is_single=True, src="/icon.png" data_image="responsive") as img:
-                div += img
+    #         with Tag("img", is_single=True, src="/icon.png" data_image="responsive") as img:
+    #             div += img
 
-            body += div
+    #         body += div
 
-        doc += body
+    #     doc += body
