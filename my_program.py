b81d841dbcde46f526e0bc5c9279737537ce78ec
KI-Shadow <ivankozlitin2012@gmail.com>
2020-10-21 00:24:05 +0300

    Code completed

--- my_program.py
+++ my_program.py
@@ -80,26 +80,27 @@ class Tag:
 
 # Генерация HTML файла
 
-with HTML(output="test.html") as doc:
-    with TopLevelTag("head") as head:
-        with Tag("title") as title:
-            title.text = "hello"
-            head += title
-        doc += head
+if __name__ == "__main__":
+    with HTML(output="test.html") as doc:
+        with TopLevelTag("head") as head:
+            with Tag("title") as title:
+                title.text = "hello"
+                head += title
+            doc += head
 
-    with TopLevelTag("body") as body:
-        with Tag("h1", klass=("main-text",)) as h1:
-            h1.text = "Test"
-            body += h1
+        with TopLevelTag("body") as body:
+            with Tag("h1", klass=("main-text",)) as h1:
+                h1.text = "Test"
+                body += h1
 
-        with Tag("div", klass=("container", "container-fluid"), id="lead") as div:
-            with Tag("p") as paragraph:
-                paragraph.text = "another test"
-                div += paragraph
+            with Tag("div", klass=("container", "container-fluid"), id="lead") as div:
+                with Tag("p") as paragraph:
+                    paragraph.text = "another test"
+                    div += paragraph
 
-            with Tag("img", is_single=True, src="/icon.png", data_image="responsive") as img:
-                div += img
+                with Tag("img", is_single=True, src="/icon.png", data_image="responsive") as img:
+                    div += img
 
-            body += div
+                body += div
 
-        doc += body
+            doc += body
